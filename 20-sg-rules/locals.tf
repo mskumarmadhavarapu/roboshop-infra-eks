b6499e5108d480dbd1292eab7906a51a01fda7fc
mskumramdhavarapu <mskumarmadhavarapu@gmail.com>
2026-04-29 23:09:28 +0530

    dockerfiles

--- 20-sg-rules/locals.tf
+++ 20-sg-rules/locals.tf
@@ -4,6 +4,7 @@ locals {
   mysql_sg_id = data.aws_ssm_parameter.mysql_sg_id.value
   redis_sg_id = data.aws_ssm_parameter.redis_sg_id.value
   rabbitmq_sg_id = data.aws_ssm_parameter.rabbitmq_sg_id.value
+  mongodb_sg_id = data.aws_ssm_parameter.mongodb_sg_id.value
   ingress_alb_sg_id = data.aws_ssm_parameter.ingress_alb_sg_id.value
   eks_control_plane_sg_id = data.aws_ssm_parameter.eks_control_plane_sg_id.value
   eks_node_sg_id = data.aws_ssm_parameter.eks_node_sg_id.value
