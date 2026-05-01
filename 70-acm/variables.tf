37378cb326211e28f477b42f9d2f8447acc17454
mskumramdhavarapu <mskumarmadhavarapu@gmail.com>
2026-05-01 21:46:46 +0530

    tf aws eks

---
+++ 70-acm/variables.tf
@@ -0,0 +1,15 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z011136426USMAK55A9XK"
+}
+
+variable "domain_name" {
+    default = "mskdaws88s.online"
+}
