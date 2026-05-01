bc003c80e92fb2d6693abef2766bd7a2cef79f22
mskumramdhavarapu <mskumarmadhavarapu@gmail.com>
2026-05-01 22:38:17 +0530

    tf aws eks

--- 80-frontend-alb/provider.tf
+++ 80-frontend-alb/provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket  = "remote-state-aws-88s-dev" # Replace with your unique bucket name
+    bucket  = "remote-state-mskdaws88s" # Replace with your unique bucket name
     key     = "roboshop-eks-alb"
     region  = "us-east-1"
     encrypt = true
