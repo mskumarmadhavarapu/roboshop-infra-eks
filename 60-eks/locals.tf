b62b89b070251522f72e6be009f968ac936f0dff
mskumramdhavarapu <mskumarmadhavarapu@gmail.com>
2026-05-02 22:04:10 +0530

    tf aws eks

--- 60-eks/locals.tf
+++ 60-eks/locals.tf
@@ -1,8 +1,8 @@
 locals {
     vpc_id = data.aws_ssm_parameter.vpc_id.value
     private_subnet_ids = split(",", data.aws_ssm_parameter.private_subnet_ids.value)[0]
-    eks_node_sg_id = local.aws_ssm_parameter.eks_node_sg_id.value
-    eks_control_plane_sg_id = local.aws_ssm_parameter.eks_control_plane_sg_id.value
+    eks_node_sg_id = data.aws_ssm_parameter.eks_node_sg_id.value
+    eks_control_plane_sg_id = data.aws_ssm_parameter.eks_control_plane_sg_id.value
     common_tags = {
         Project = var.project
         Environment = var.environment
